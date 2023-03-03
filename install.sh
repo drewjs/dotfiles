4b6d61b05f606087f95ec4b3508dfd810639d171
Andrew Stalker <andrew@bitcoinira.com>
2023-03-03 12:25:01 -0800

    update install

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 chsh -s $(which zsh)
 
-sudo apt-get update && sudo apt-get upgrade -y
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
