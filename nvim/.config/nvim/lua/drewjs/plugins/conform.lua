a1adf0e33cd1a38ff61014d37e438cbd16349228
Andrew Galvan <drewjsdev@pm.me>
2024-05-15 20:59:48 -0700

    nvim updates

--- nvim/.config/nvim/lua/drewjs/plugins/conform.lua
+++ nvim/.config/nvim/lua/drewjs/plugins/conform.lua
@@ -13,42 +13,63 @@ return {
 				desc = "[F]ormat buffer",
 			},
 		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				local disable_filetypes = {
-					c = true,
-					cpp = true,
-					javascript = true,
-					javascriptreact = true,
-					typescript = true,
-					typescriptreact = true,
-				}
+		config = function()
+			local conform = require("conform")
 
-				return {
-					timeout_ms = 1000,
-					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-				}
-			end,
-			formatters_by_ft = {
-				html = { { "prettierd", "prettier" } },
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				jsonc = { { "prettierd", "prettier" } },
-				lua = { "stylua" },
-				markdown = { { "prettierd", "prettier" } },
-				typescript = { { "biome", "prettierd", "prettier" } },
-				typescriptreact = { { "biome", "prettierd", "prettier" } },
-			},
-			formatters = {
-				prettierd = {
-					env = {
-						PRETTIERD_LOCAL_PRETTIER_ONLY = "1",
-					},
+			local js_format = {
+				{ "prettierd", "prettier" },
+				"biome",
+			}
+
+			conform.setup({
+				format_on_save = function(bufnr)
+					local disable_filetypes = {
+						c = true,
+						cpp = true,
+						javascript = true,
+						javascriptreact = true,
+						typescript = true,
+						typescriptreact = true,
+					}
+
+					-- Disable with a global or buffer-local variable
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+
+					return {
+						timeout_ms = 1000,
+						lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+					}
+				end,
+				formatters_by_ft = {
+					javascript = js_format,
+					javascriptreact = js_format,
+					lua = { "stylua" },
+					typescript = js_format,
+					typescriptreact = js_format,
 				},
-			},
-		},
+			})
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
+		end,
 	},
 }
 -- vim: ts=2 sts=2 sw=2 et
